0fc7e12b23df7171b310925dc4707311f8acbf2d
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-09-18 13:36:40 +0200

    draft of sign up form

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,7 +1,38 @@
+import { useState } from 'react'
 import LayoutRoot from '../LayoutRoot'
 
 const Auth = () => {
- return <LayoutRoot><h1>Auth</h1></LayoutRoot>
+  const [formData, setFormData] = useState({ email: '', password: '', })
+
+  const handleChange = e => {
+    const {name, value} = e.target
+    setFormData({ ...formData,
+      [name]: value,
+    })
+  }
+  
+  const handleSubmit = (e) => {
+  e.preventDefault()
+
+  console.log(formData)
+
+  setFormData({
+    email: '',
+    password: '',
+  })
+}
+  return (
+    <LayoutRoot bgImage='images/auth-bg'  heading='CREATE ACCOUNT'>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Email:</label>
+          <input type='email' name='email' value={formData.email} onChange={handleChange} required />
+          <label>Password:</label>
+          <input type='password' name='password' value={formData.password} onChange={handleChange} required />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </LayoutRoot>)
 }
 
 export default Auth
