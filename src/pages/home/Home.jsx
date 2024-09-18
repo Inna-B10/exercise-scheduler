7bf76d0dbb7540cb474b3bddc2fabbcb07b939b2
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-09-18 12:30:34 +0200

    added images, created button, styled Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+import Button from '../../components/ui/button/Button'
+import useAuth from '../../hooks/useAuth'
 import LayoutRoot from './../LayoutRoot'
+import styles from './Home.module.scss'
+
 const Home = () => {
+const { isAuth } = useAuth()
+const navigate = useNavigate()
 
 	return (
-		<LayoutRoot>
-
-				<h1>Home page</h1>
-
+		<LayoutRoot bgImage='images/home-bg.jpg'>
+			<Button clickHandler={() => navigate(!isAuth ? '/auth' : '/new-workout')}>
+				{!isAuth? 'Sign in': 'New'}
+			</Button>
+			<h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
+			{/* TODO: Counters */}
 		</LayoutRoot>
     )
 }
