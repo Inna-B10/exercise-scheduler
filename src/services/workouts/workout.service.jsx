fdfb6bb3059dfd0fbd0400515a305aa614022a66
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-10-01 15:02:04 +0200

    added WorkoutService

---
+++ src/services/workouts/workout.service.jsx
@@ -0,0 +1,27 @@
+import { $axios } from '../../api'
+
+const WORKOUTS = '/workouts'
+
+class WorkoutService {
+	async getAll() {
+		return $axios.get(WORKOUTS)
+	}
+	async getById(id) {
+		return $axios.get(`${WORKOUTS}/${id}`)
+	}
+
+	// name, exerciseIds
+	async create(body) {
+		return $axios.post(WORKOUTS, body)
+	}
+
+	async update(id, body) {
+		return $axios.put(`${WORKOUTS}/${id}`, body)
+	}
+
+	async delete(id) {
+		return $axios.delete(`${WORKOUTS}/${id}`)
+	}
+}
+
+export default new WorkoutService()
