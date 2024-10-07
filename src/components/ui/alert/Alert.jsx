a9bd22bc757e5af99ae8edfd18d7d6a4f0a7bd66
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-10-07 18:52:34 +0200

    small fixes: Alert message, bg images

--- src/components/ui/alert/Alert.jsx
+++ src/components/ui/alert/Alert.jsx
@@ -2,6 +2,9 @@ import cn from 'clsx'
 import styles from './Alert.module.scss'
 
 const Alert = ({ type = 'success', text }) => {
+	if (text.startsWith('\n')) {
+		text = 'Something went wrong!'
+	}
 	return <div className={cn(styles.alert, styles[type])}>{text}</div>
 }
 
