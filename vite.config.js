4f72649695693d0d9d1c13ccf8f6574aa2172cf3
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-10-07 18:49:18 +0200

    fix for deprecated legacy JS API

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler' // or "modern", "legacy"
+			}
+		}
+	},
 	plugins: [react()],
 	base: '/exercise-scheduler/'
 })
